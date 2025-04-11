bfcb9e535ba8b689fac865765bfe84586b739660
Felix Lopez <flopezmenardi@itba.edu.ar>
2025-04-11 12:05:27 -0300

    Fix selection strategy error

--- ga/selection/__init__.py
+++ ga/selection/__init__.py
@@ -2,11 +2,13 @@ from .tournament import tournament_selection
 from .roulette import roulette_selection
 from .ranking import ranking_selection
 from .boltzmann import boltzmann_selection
+from .elite import elite_selection
 
 # Mapeo por string => función
 selection_strategies = {
     "tournament": tournament_selection,
     "roulette": roulette_selection,
     "ranking": ranking_selection,
-    "boltzmann": boltzmann_selection
+    "boltzmann": boltzmann_selection,
+    "elite": elite_selection
 }
