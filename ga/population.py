39cd387a4bedbd1fe59cdd93510d380e206aa3bc
tmmymrtnz <tommy40@me.com>
2025-04-12 20:26:30 +0200

    Triangles start with a color sample from the image

--- ga/population.py
+++ ga/population.py
@@ -2,14 +2,14 @@ import random
 from .individual import Individual
 
 class Population:
-    def __init__(self, config, image_width, image_height):
+    def __init__(self, config, image_width, image_height, target_image=None):
         """
         Creates an initial population based on the provided configuration.
-        Here, config["n_triangles"] is expected to be the number of triangles per patch.
+        Passes target_image to Individual for color initialization.
         """
         self.individuals = []
         for _ in range(config["population_size"]):
-            ind = Individual(config["n_triangles"], image_width, image_height)
+            ind = Individual(config["n_triangles"], image_width, image_height, target_image)
             self.individuals.append(ind)
 
     def evaluate(self, fitness_func, target_image):
